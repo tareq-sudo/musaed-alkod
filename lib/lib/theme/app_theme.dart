e20bb64cf84cd39655fc795a75acf37efdad9692
tareq-sudo <tark46743@gmail.com>
2026-03-08 12:04:00 +0300

    Add AppTheme class with color constants

---
+++ lib/lib/theme/app_theme.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class AppTheme {
+  static const Color primary = Color(0xFF6C63FF);
+  static const Color secondary = Color(0xFF03DAC6);
+  static const Color background = Color(0xFF0F0F1A);
+  static const Color surface = Color(0xFF1A1A2E);
+  static const Color cardColor = Color(0xFF16213E);
+  static const Color textPrimary = Color(0xFFFFFFFF);
+  static const Color textSecondary = Color(0xFFB0B0C3);
+}
